b2ade2051f251f4795625d535b64ab0d53ae46e1
Pranay Yelugam <shruthiy@SHRUTHIYs-MacBook-Pro.local>
2021-04-23 20:24:43 -0400

    updated docker commands

--- docker/run_docker_images.sh
+++ docker/run_docker_images.sh
@@ -1,13 +1,22 @@
-docker network create myNetwork &&
+docker pull pranayyelugam/frontend &&
+docker pull pranayyelugam/catalog &&
+docker pull pranayyelugam/order &&
+docker pull pranayyelugam/loadbalancer &&
+
+docker network create pygmy &&
 sleep 1
-docker run --network=myNetwork -p 8080:8080 --name loadbalancer lb &
+docker run --network=pygmy -p 8080:8080 --name loadbalancer pranayyelugam/loadbalancer &
 sleep 7
-docker run --network=myNetwork -p 8081:8081 --name frontend fs &
+docker run --network=pygmy -p 8081:8081 --name frontend pranayyelugam/frontend &
 sleep 2
-docker run --network=myNetwork -p 8082:8082 --name catalog-1 --env PORT=8082 --env HOST=catalog-1 cs  &
+docker run --network=pygmy -p 8082:8082 --name catalog-1 --env PORT=8082 --env HOST=catalog-1 pranayyelugam/catalog  &
 sleep 2
-docker run --network=myNetwork -p 8083:8083 --name catalog-2 --env PORT=8083 --env HOST=catalog-2  cs &
+docker run --network=pygmy -p 8083:8083 --name catalog-2 --env PORT=8083 --env HOST=catalog-2  pranayyelugam/catalog &
 sleep 2
-docker run --network=myNetwork -p 8084:8084 --name order-1 --env PORT=8084 --env HOST=order-1 os &
+docker run --network=pygmy -p 8084:8084 --name order-1 --env PORT=8084 --env HOST=order-1 pranayyelugam/order &
 sleep 2
-docker run --network=myNetwork -p 8085:8085 --name order-2 --env PORT=8085 --env HOST=order-2 os
+docker run --network=pygmy -p 8085:8085 --name order-2 --env PORT=8085 --env HOST=order-2 pranayyelugam/order &
+sleep 5
+python ../client-process/client.py http://0.0.0.0:8081 &
+sleep 5
+python ../tests/tests.py http://0.0.0.0:8081 http://0.0.0.0:8082 &
--- docker/src/order-server/order.py
+++ docker/src/order-server/order.py
@@ -1,44 +1,53 @@
 from flask import Flask
 import json, sys, requests, random
+
 app = Flask(__name__)
 
 
-@app.route('/')
+@app.route("/")
 def checkAlive():
-    print("hello")
     return "Hey! I'm well and alive. Don't worry about me"
 
+
 # ROUTE: /buy/itemNumber
-@app.route('/buy/<int:itemNumber>')
+@app.route("/buy/<int:itemNumber>")
 def buyRequestForItem(itemNumber):
-    queryResponse = requests.get(app.config.get('loadbalancer_uri') + '/@query@' + str(itemNumber)).json()
+    queryResponse = requests.get(
+        app.config.get("loadbalancer_uri") + "/@query@" + str(itemNumber)
+    ).json()
     if queryResponse:
         stock = int(queryResponse[0]["stock"]) - 1
-        if (stock >= 0):
+        if stock >= 0:
             try:
-                updateResponse =  requests.get(app.config.get('loadbalancer_uri') + '/@reduceStock@'+ str(itemNumber))
+                updateResponse = requests.get(
+                    app.config.get("loadbalancer_uri")
+                    + "/@reduceStock@"
+                    + str(itemNumber)
+                )
                 updateResponse.raise_for_status()
             except HTTPError as http_err:
-                print(f'HTTP error occurred: {http_err}') 
+                print(f"HTTP error occurred: {http_err}")
             except Exception as err:
-                print(f'Other error occurred: {err}')  
+                print(f"Other error occurred: {err}")
             else:
                 return updateResponse.content
         else:
-            print("Stock for item number %d is less than 0" % itemNumber )
+            print("Stock for item number %d is less than 0" % itemNumber)
             return {}
     else:
         return {}
-        
+
 
 if __name__ == "__main__":
-    app.config['loadbalancer_uri'] = sys.argv[1]
-    app.config['host'] = sys.argv[2]
-    app.config['port'] = sys.argv[3]
+    app.config["loadbalancer_uri"] = sys.argv[1]
+    app.config["host"] = sys.argv[2]
+    app.config["port"] = sys.argv[3]
 
-    host = app.config['host']
-    port = app.config['port']
-    
-    res = requests.get(app.config.get('loadbalancer_uri') + '/@register_order@' + host + ':' + port)
+    host = app.config["host"]
+    port = app.config["port"]
+
+    res = requests.get(
+        app.config.get("loadbalancer_uri") + "/@register_order@" + host + ":" + port
+    )
 
     app.run(host=host, port=port)
